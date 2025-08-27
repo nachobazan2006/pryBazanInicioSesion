c070b7ae504e089a65f346d9301a251b6493de69
nachoBazan <nacho68bazan@gmail.com>
2025-08-27 12:53:54 -0300

    Agregue los comandos necesarios

--- pryBazanInicioSesion/Form1.Designer.cs
+++ pryBazanInicioSesion/Form1.Designer.cs
@@ -73,7 +73,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 4;
-            button1.Text = "Aceptar";
+            button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // Form1
